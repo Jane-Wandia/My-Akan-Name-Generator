02b49f838747b2af94b7bb5105fc6d3ce728e81a
Jane-Wandia <janekigenyi2018@gmail.com>
2022-03-07 16:33:59 +0300

    added a link, a div and a ul

---
+++ index.js
@@ -0,0 +1,30 @@
+function submitFore(event);{
+    event .preventDefault();
+    var dat =documents.form[
+        "akanForm"
+    ]["*birthday"].valeu;
+    var gender =documents.forms[
+        "akanForm"    
+    ]["gender"].value;
+    var akanName ="";
+}
+
+var femaleNames =[
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+];
+var maleNames =[
+    "Kwasi",
+    "Kudwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+];
