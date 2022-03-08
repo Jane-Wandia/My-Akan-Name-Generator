3bca2b599ab09cc21898db86fb36eb1505329b20
Jane-Wandia <janekigenyi2018@gmail.com>
2022-03-08 11:10:08 +0300

    added a readme

--- index.js
+++ index.js
@@ -32,15 +32,41 @@ var day =dateObject.getDay();
 if (day !=null && gender != null){
     if (gender === "male"){
         akanName = maleNames[day];
-    };
+    }
     
-};
 
 else{
     akanName = femaleNames[day];
+}
 }
 
 document.getElementById(
     "akanNames").innerHTML= akanName;
 
+    ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )
 
+   // CC - is the century digits. For example 1989 has CC = 19
+
+  //  YY - is the Year digits (1989 has YY = 89)
+   
+  //  MM -  is the Month
+   
+  //  DD - is the Day of the month 
+   
+  //  mod - is the modulus function ( % )
+
+var submit=document.getElementById("submit");
+
+submit.addEventListener("click", ()=>{
+    var userMonth=document.getElementById("enterMonth").value;
+    var userDay=document.getElementById("enterDay").value
+    var userName=document.getElementById("enterName").value;
+
+    var yeaBirth=userDay/userMonth/userName;
+
+    console.log(yearBirth);
+
+
+
+    var userName=document.getElementById("userOutput");
+    userOutput.innertext="Your name is" + " " + userName;
